cb7af316c6e593c3f14c5f236075fd51d109296e
Said Eloudrhiri <info@noratek.net>
2017-12-01 21:57:45 +0100

    Change network name and port

--- truffle.js
+++ truffle.js
@@ -2,9 +2,9 @@ module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
      networks: {
-          ganache: {
+          development: {
                host: "localhost",
-               port: 7545,
+               port: 8545,
                network_id: "*" // Match any network id
           }
      }
