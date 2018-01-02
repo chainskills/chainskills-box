7808ba346d6112678fe706ba163188c131af2dbb
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2018-01-02 11:38:19 +0100

    Update truffle.js
    Replace the development network with a ganache network since we said earlier in the course that we should not name a network "development"

--- truffle.js
+++ truffle.js
@@ -2,10 +2,10 @@ module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
      networks: {
-          development: {
+          ganache: {
                host: "localhost",
-               port: 8545,
-               network_id: "*" // Match any network id
+               port: 7545,
+               network_id: "5777"
           }
      }
 };
